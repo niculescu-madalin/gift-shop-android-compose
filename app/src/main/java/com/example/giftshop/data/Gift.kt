aca606759987fed7f5782dc10133809a0a1f4b59
Maria Irina Niculescu <maria.irina.niculescu@gmail.com>
2025-03-29 17:24:32 +0200

    Changed imageUrl to imageId for using image resources in Gift Data Class

--- app/src/main/java/com/example/giftshop/data/Gift.kt
+++ app/src/main/java/com/example/giftshop/data/Gift.kt
@@ -4,5 +4,5 @@ data class Gift(
     val id: Int,
     val name: String,
     val price: Double,
-    val imageUrl: String, // Or a drawable resource ID if using local images
+    val imageId: Int, // Or a drawable resource ID if using local images
 )
