5a893651527bd7651d408a9d908f8916c7879a7f
Maria Irina Niculescu <maria.irina.niculescu@gmail.com>
2025-03-29 16:40:24 +0200

    Added a page for a single product

---
+++ app/src/main/java/com/example/giftshop/ProductPage.kt
@@ -0,0 +1,24 @@
+package com.example.giftshop
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+/* Page for a single Product  */
+@Composable
+fun ProductPage(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(text = "Product Page")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductPagePreview() {
+    HomePage()
+}
